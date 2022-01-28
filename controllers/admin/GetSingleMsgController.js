c6826795f11da46a76ca30da584cf8a22acd7fe5
kar-thee <ourworldourpeople@gmail.com>
2022-01-28 23:05:12 +0530

    developing msgBox

--- controllers/admin/GetSingleMsgController.js
+++ controllers/admin/GetSingleMsgController.js
@@ -3,10 +3,17 @@ const ContactForm = require("../../models/ContactForm");
 const GetSingleMsgController = async (req, res) => {
   const { id } = req.params;
   try {
-    const msg = await ContactForm.findOne({ id });
-    res.send({ msg, type: "success" });
+    const msgFound = await ContactForm.findOne({ id });
+    if (!msgFound) {
+      return res.status(404).send({ msg: e.message, type: "error" });
+    }
+    res.send({
+      msgFound,
+      msg: "Successfully Fetched Message",
+      type: "success",
+    });
   } catch (e) {
-    res.send({ msg: e.message, type: "error" });
+    res.status(500).send({ msg: e.message, type: "error" });
   }
 };
 module.exports = GetSingleMsgController;
