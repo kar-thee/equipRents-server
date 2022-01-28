facea4a56178a4a96cc30281796213849a5e7f41
kar-thee <ourworldourpeople@gmail.com>
2022-01-28 22:27:50 +0530

    developing msgBox

--- models/ContactForm.js
+++ models/ContactForm.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 
-const contactSchema = mongoose.Schema({
-  name: {
-    type: String,
-  },
-  email: {
-    type: String,
-    validate: {
-      validator: function (v) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+const contactSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email",
       },
-      message: "Please enter a valid email",
+    },
+    phoneNo: {
+      type: String,
+    },
+    query: {
+      type: String,
     },
   },
-  phoneNo: {
-    type: String,
-  },
-  query: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const ContactForm = mongoose.model(
   "ContactForm",
